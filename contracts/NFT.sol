79ad4de4166566d497189ec4bfa6a831f6ea7566
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-05-20 15:33:38 -0700

    added NFT minting contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,4 +7,16 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract NFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
+
+    constructor() ERC721("NFTTutorial", "NFT") {}
+
+    function mintTo(address recipient)
+        public
+        returns (uint256)
+    {
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
 }
