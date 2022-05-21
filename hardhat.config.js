01cfa4e2083a11ede1daaba05f7e5057ef363c64
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-05-21 11:58:30 -0700

    created hardhat config file, successfully compiled contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
+
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.1",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    polygon: {
+      chainId: 1,
+      url: `https://polygon-rpc.com/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+  },
 };
